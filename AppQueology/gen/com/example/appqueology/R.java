b69dc036d94189f87b3f7a990bdcf5b5a1e6b932
qfornaguera <qfornaguera@gmail.com>
2014-05-15 18:46:36 +0200

    #26 timeline form layout

--- AppQueology/gen/com/example/appqueology/R.java
+++ AppQueology/gen/com/example/appqueology/R.java
@@ -1003,11 +1003,11 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050052;
+        public static final int action_settings=0x7f050053;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int activity_main=0x7f05004a;
         public static final int always=0x7f05000b;
-        public static final int beautify=0x7f050054;
+        public static final int beautify=0x7f050055;
         public static final int beginning=0x7f050011;
         public static final int button1=0x7f05004f;
         public static final int checkbox=0x7f05002d;
@@ -1021,6 +1021,7 @@ containing a value of this type.
         public static final int dropdown=0x7f05000f;
         public static final int editText1=0x7f05003d;
         public static final int editText2=0x7f050044;
+        public static final int editText3=0x7f050050;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
@@ -1034,7 +1035,7 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
-        public static final int load=0x7f050053;
+        public static final int load=0x7f050054;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1070,8 +1071,8 @@ containing a value of this type.
         public static final int textView4=0x7f050041;
         public static final int textView5=0x7f050042;
         public static final int textView6=0x7f050043;
-        public static final int timeline=0x7f050051;
-        public static final int timelinebody=0x7f050050;
+        public static final int timeline=0x7f050052;
+        public static final int timelinebody=0x7f050051;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1129,13 +1130,15 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int activity_artifact=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int activity_time_line=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int activity_threshold=0x7f030019;
+        public static final int activity_time_line=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
         public static final int artifact=0x7f0c0000;
         public static final int main=0x7f0c0001;
-        public static final int time_line=0x7f0c0002;
+        public static final int threshold=0x7f0c0002;
+        public static final int time_line=0x7f0c0003;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1183,6 +1186,7 @@ containing a value of this type.
         public static final int hello_world=0x7f0a000f;
         public static final int title_activity_artifact=0x7f0a0011;
         public static final int title_activity_prova=0x7f0a0010;
+        public static final int title_activity_threshold=0x7f0a0013;
         public static final int title_activity_time_line=0x7f0a0012;
     }
     public static final class style {
